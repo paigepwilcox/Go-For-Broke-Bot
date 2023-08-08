9bd7f498e83715625c97ae9c0c8f20d6bff9195b
Paige <ph.paigee@gmail.com>
2023-08-08 14:18:35 -0400

    deleted comments

--- contracts/FlashLoanArbitrage.sol
+++ contracts/FlashLoanArbitrage.sol
@@ -7,23 +7,10 @@ pragma abicoder v2;
 // import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
-/* 
-flashloansimplereceiverbase - we need to implement this interface for our contract to be a receiver of a loan. contains the executeOperation() 
-*/
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
-
 import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
-/* 
-IPoolAdressesProvider is called within our flashloansimplereceiverbase
-* what are we using from this import?
-*/
 import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
-/* 
-IERC20 is an interface 
-* what are we using from this import? calling the approve function on the token we are borrowing 
-*/
 interface IUniswapV2Router {
 // https://docs.uniswap.org/contracts/v2/reference/smart-contracts/library#getamountsout
     function getAmountsOut(uint256 amountIn, address[] memory path) external view returns (uint256[] memory amounts); // returns the maximum output amount of the other asset (accounting for fees) given reserves.
@@ -38,19 +25,7 @@ contract FlashLoanArbitrage is FlashLoanSimpleReceiverBase, Ownable {
     address router2;
     address token1;
     address token2;
-
-    // i think i have to make these state variable to be able to update their values
     
-    /* 
-    * an address type variable named owner, payable allows owner to receive ether to a contract
-    * When a developer explicitly marks a smart contract with the payable type, they are saying “I expect ether to be sent to this function”. 
-    */
-
-
-    /* 
-    * a constructor function is used to initialize state variables of a contract 
-    * a contract has only one constructor
-    * since this contract is inheriting from FlashLoanSimpleReceiverBase the constructor is initializing an instance of this contract */
     constructor(address _addressProvider) 
         FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider))
     {
@@ -106,10 +81,7 @@ contract FlashLoanArbitrage is FlashLoanSimpleReceiverBase, Ownable {
         address initiator,
         bytes calldata params
     ) external override onlyOwner returns (bool) {
-        /*since we are inheriting we need to put "override" 
-
-        ** at this point in our code we have sucessfully borrowed funds 
-        ** custom logic for arbitrage */
+        
         uint startBalance = IERC20(token1).balanceOf(address(this));
         uint token2InitialBalance = IERC20(token2).balanceOf(address(this));
         swap(router1, token1, token2, amount);
@@ -119,37 +91,20 @@ contract FlashLoanArbitrage is FlashLoanSimpleReceiverBase, Ownable {
         uint endBalance = IERC20(token1).balanceOf(address(this));
         require(endBalance > startBalance, "reverted, you're broke");
 
-        // set up amount owed - amount that we need to approve for the pool contract to take back
+        
         uint256 amountOwed = amount + premium;
-        // using ierc20 to pass in the asset and call the approve function as stated above
-        // POOL is coming from FLashLoanSimpleReceiverBase.sol
         IERC20(asset).approve(address(POOL), amountOwed);
 
         return true;
     }
     
-    
-    /*
-    * use this at the very end to see what the balance of our contract is
-    * external defines a type of function that can only be called outside the contract by other contracts
-    * view defines a function that reads state variables but does not alter them
-     */
-    function getBalance(address _tokenAddress) external view returns (uint256) {
-        // instantiate with _tokenAddress
-        // gives us this contracts balance of any token we specify 
+
+    function getBalance(address _tokenAddress) external view returns (uint256) { 
         return IERC20(_tokenAddress).balanceOf(address(this));
     }
 
-
-    // need a function to withdraw our profit funds
-    // designed to withdraw specific tokens
     function withdraw(address _tokenAddress) external onlyOwner {
         IERC20 token = IERC20(_tokenAddress);
-
-        /*
-        * initiate transfer on this token object
-        * msg.sender is the owner of the contract 
-        * token.balanceOf -- all the holdings of this contract for this particular token */
         token.transfer(msg.sender, token.balanceOf(address(this)));
     }
 
