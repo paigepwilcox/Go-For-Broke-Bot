872dde809e8bf353816218ab05fe1b0f6c441b82
Paige <ph.paigee@gmail.com>
2024-02-19 18:49:45 -0500

    commented functions

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -32,6 +32,9 @@ contract Dex {
         usdc = IERC20(usdcAddress);
     }
 
+    /// Deposit USDCS tokens into contract.
+    /// @param _amount amount to be transfered into this contract which serves as a simulated exchange 
+    /// @dev use .allowance() to allow your contract to accept funds from 'msg.sender' and transferFrom() to execute transaction
     function depositUSDC(uint256 _amount) external {
         usdcBalances[msg.sender] += _amount;
         uint256 allowance = usdc.allowance(msg.sender, address(this));
@@ -39,6 +42,9 @@ contract Dex {
         usdc.transferFrom(msg.sender, address(this), _amount);
     }
 
+    /// Deposits WETH tokens into contract.
+    /// @param _amount amount to be transfered into this contract which serves as a simulated exchange 
+    /// @dev use .allowance() to allow your contract to accept funds from 'msg.sender' and transferFrom() to execute transaction
     function depositWETH(uint256 _amount) external {
         wethBalances[msg.sender] += _amount;
         uint256 allowance = weth.allowance(msg.sender, address(this));
@@ -46,27 +52,34 @@ contract Dex {
         weth.transferFrom(msg.sender, address(this), _amount);
     }
 
+    /// Transfers USDC for WETH from this contract to `msg.sender`
     function buyWETH() external {
         uint256 wethToReceive = ((usdcBalances[msg.sender] / dexARate) * 100) *
             (10**12);
         weth.transfer(msg.sender, wethToReceive);
     }
 
+    /// Transfers WETH for USDC from this contract to `msg.sender`
     function sellWETH() external {
         uint256 usdcToReceive = ((wethBalances[msg.sender] * dexBRate) / 100) /
             (10**12);
         usdc.transfer(msg.sender, usdcToReceive);
     }
 
+    /// Gets the balance of a token within a contract
+    /// @param _tokenAddress address of a standard ERC20 token 
     function getBalance(address _tokenAddress) external view returns (uint256) {
         return IERC20(_tokenAddress).balanceOf(address(this));
     }
 
+    /// Allows the owner to withdraw a token into `msg.sender`
+    /// @param _tokenAddress address of a standard ERC20 token
     function withdraw(address _tokenAddress) external onlyOwner {
         IERC20 token = IERC20(_tokenAddress);
         token.transfer(msg.sender, token.balanceOf(address(this)));
     }
 
+    /// Modifer that allows functions to be called only by the `msg.sender`
     modifier onlyOwner() {
         require(
             msg.sender == owner,
@@ -75,5 +88,6 @@ contract Dex {
         _;
     }
 
+    /// Allows this contract to receive funds
     receive() external payable {}
 }
